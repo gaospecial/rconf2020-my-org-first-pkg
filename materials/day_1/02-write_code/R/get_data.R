f4a8494f31bac28af9ad006aff77ec20530a9eef
Chun-Hui Gao <gaospecial@gmail.com>
2021-01-13 20:21:16 +0800

    为函数添加一个参数

--- materials/day_1/02-write_code/R/get_data.R
+++ materials/day_1/02-write_code/R/get_data.R
@@ -1,5 +1,8 @@
-get_resident_data <- function() {
+get_resident_data <- function(data_table = FALSE) {
   residents_per_sector <- db_con("residents_per_sector")
-
-  tibble::as_tibble(residents_per_sector)
+  if (data_table) {
+    data.table::as.data.table(residents_per_sector)
+  } else {
+    tibble::as_tibble(residents_per_sector)
+  }
 }
