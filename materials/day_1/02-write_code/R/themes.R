b6b30bf998ef03be085db514c7e9b9dd73c7f5d4
Chun-Hui Gao <gaospecial@gmail.com>
2021-01-13 20:20:02 +0800

    定义一个 ggplot 主题

--- materials/day_1/02-write_code/R/themes.R
+++ materials/day_1/02-write_code/R/themes.R
@@ -1,5 +1,9 @@
 theme_avalanche <- function() {
-  library(ggplot2)
-  theme_minimal(base_size = 14) +
-    theme(panel.grid.minor = element_blank())
+  ggplot2::theme_minimal(base_size = 14) +
+    ggplot2::theme(panel.grid.minor = element_blank())
+}
+
+theme_avalanche_h <- function(){
+  ggplot2::theme_minimal(base_size = 14) +
+    ggplot2::theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank())
 }
